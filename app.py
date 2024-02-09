2ac481c8520478e35916aae5439dfa1bc009703d
jifanizz <jifanizz@cisco.com>
2024-02-08 22:17:07 -0500

    second upload

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import faiss
+
+
+ 
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+        )
+
+def VectorStore(text_chunks):
+
+    
+    chunks = text_splitter.split_text(text)
+    return chunks
+    
 
 
 
@@ -11,8 +41,20 @@ def main():
 
     with st.sidebar:
         st.subheader("Upload Documents here:")
-        st.file_uploader("Upload PDFs here:", type="pdf", accept_multiple_files=True)
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload PDFs here:", type="pdf", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+               
+                # get pdf
+                raw_text = get_pdf_text(pdf_docs)
+                #st.write(raw_text)
+
+                # get text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+
+                # create vector store
+                vectorstore = VectorStore(text_chunks)
 
 
 
